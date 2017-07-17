1852d61b26c2d5c9a1997b4fcd6e23f700eab9fb
ST <clerfayt@gmx.at>
2017-07-17 15:14:05 +0200

    work in progress: zoom

--- app/src/main/java/com/github/clerfayt/imageviewer/ZoomImageView.java
+++ app/src/main/java/com/github/clerfayt/imageviewer/ZoomImageView.java
@@ -8,7 +8,7 @@ import android.widget.ImageView;
 
 public class ZoomImageView extends ImageView {
 
-    private int zoomControler = 20;
+    private int zoomControler = 0;
 
     public ZoomImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -17,26 +17,24 @@ public class ZoomImageView extends ImageView {
         super(context);
     }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        this.getDrawable().setBounds((getWidth()/2)-zoomControler, (getHeight()/2)-zoomControler, (getWidth()/2)+zoomControler, (getHeight()/2)+zoomControler);
-        this.getDrawable().draw(canvas);
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        if(keyCode == KeyEvent.KEYCODE_DPAD_UP){ // zoom in
-            zoomControler += 10;
-        }
-        if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){ // zoom out
-            zoomControler -= 10;
-            if(zoomControler < 10) zoomControler = 10;
-        }
-
-        invalidate();
-        return true;
-    }
+//    @Override
+//    protected void onDraw(Canvas canvas) {
+//        super.onDraw(canvas);
+//        if (zoomControler < 20) zoomControler = Math.min(getWidth()/2, getHeight()/2);
+//        this.getDrawable().setBounds((getWidth()/2)-zoomControler, (getHeight()/2)-zoomControler, (getWidth()/2)+zoomControler, (getHeight()/2)+zoomControler);
+//    }
+//
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//
+//        if(keyCode == KeyEvent.KEYCODE_DPAD_UP){ // zoom in
+//            zoomControler += 10;
+//        }
+//        if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){ // zoom out
+//            zoomControler -= 10;
+//        }
+//
+//        invalidate();
+//        return true;
+//    }
 }
